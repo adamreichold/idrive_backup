5cbdcadbd680f19b15256f5f82e91f5a67148f57
Adam Reichold <adam.reichold@t-online.de>
2019-04-21 19:08:00 +0200

    Use a systemd user service to remove quite a bit of redundant functionality

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,154 @@
-use std::env::{args_os, set_current_dir};
 use std::ffi::{OsStr, OsString};
-use std::fmt::Arguments;
-use std::fs::{
-    create_dir_all, read_dir, read_to_string, remove_file, set_permissions, write, File,
-    OpenOptions, Permissions,
-};
+use std::fs::{remove_file, set_permissions, write, File, Permissions};
 use std::io::{BufWriter, Write};
 use std::os::unix::ffi::OsStrExt;
 use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
-use std::thread::sleep;
-use std::time::{Duration, SystemTime};
 
-use failure::{bail, Error, SyncFailure};
-use humantime::{format_rfc3339_seconds, parse_rfc3339};
-use log::{error, info, warn, Level, Log, Record};
+use chrono::{offset::Local, DateTime};
+use failure::{bail, Error, ResultExt, SyncFailure};
+use log::{error, info, warn};
 use serde::de::DeserializeOwned;
 use serde_derive::Deserialize;
 use serde_xml_rs::from_str as from_xml_str;
 use serde_yaml::from_reader as from_yaml_reader;
-use simplelog::{Config as LogConfig, LevelFilter, SharedLogger, WriteLogger};
+use simplelog::{Config as LogConfig, LevelFilter, SimpleLogger};
 use tempfile::{NamedTempFile, TempDir};
+
+fn main() -> Result<(), Error> {
+    SimpleLogger::init(
+        LevelFilter::Info,
+        LogConfig {
+            time: None,
+            target: None,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    download_util().context("Failed to download idevsutil_dedup")?;
+
+    let config = read_config().context("Failed to read config")?;
+    let srv_ip = get_server_ip(&config).context("Failed to determine server IP")?;
+    let dev_id = get_device_id(&config, &srv_ip).context("Failed to determine device ID")?;
+
+    info!(
+        "Starting backup from {} to {} ({}) at {}...",
+        get_hostname()?,
+        config.device_name,
+        dev_id,
+        srv_ip
+    );
+
+    let starttime = Local::now();
+
+    let mut paths = config.includes.clone();
+    let mut files = Vec::new();
+    let mut stats = FileStats::default();
+
+    while let Some(path) = paths.pop() {
+        let path = match path.canonicalize() {
+            Ok(path) => path,
+            Err(err) => {
+                warn!(
+                    "Skipping path {} as it appears to be a broken symbolic link: {}",
+                    path.display(),
+                    err
+                );
+                continue;
+            }
+        };
+
+        if let Some(exclude) = config
+            .excludes
+            .iter()
+            .find(|exclude| path.starts_with(exclude))
+        {
+            info!(
+                "Skipping path {} due to exclude {}",
+                path.display(),
+                exclude.display(),
+            );
+            continue;
+        }
+
+        if path.is_file() {
+            files.push(path);
+
+            if files.len() == 1000 {
+                upload_files(&config, &srv_ip, &dev_id, &mut stats, &files)
+                    .context("Failed to upload files")?;
+                files.clear();
+            }
+        } else if path.is_dir() {
+            let dir = match path.read_dir() {
+                Ok(dir) => dir,
+                Err(err) => {
+                    warn!(
+                        "Skipping directory {} as it appears to have been removed: {}",
+                        path.display(),
+                        err
+                    );
+                    continue;
+                }
+            };
+
+            for entry in dir {
+                let entry = match entry {
+                    Ok(entry) => entry,
+                    Err(err) => {
+                        warn!(
+                            "Skipping entry in directory {} as it appears to have been removed: {}",
+                            path.display(),
+                            err
+                        );
+                        continue;
+                    }
+                };
+
+                paths.push(entry.path());
+            }
+        } else {
+            warn!(
+                "Skipping path {} as it is neither a file nor a directory",
+                path.display()
+            );
+            continue;
+        }
+    }
+
+    if !files.is_empty() {
+        upload_files(&config, &srv_ip, &dev_id, &mut stats, &files)
+            .context("Failed to upload files")?;
+    }
+
+    let endtime = Local::now();
+
+    if stats.failed_to_backup != 0 {
+        error!(
+            "Failed to backup {} out of {} files",
+            stats.failed_to_backup, stats.considered_for_backup
+        );
+    } else {
+        info!("Finished backup of {} files", stats.considered_for_backup);
+    }
+
+    mail_summary(&config, &srv_ip, &starttime, &endtime, &stats)
+        .context("Failed to mail summary")?;
+
+    Ok(())
+}
 
 #[derive(Deserialize)]
 struct Config {
-    interval: u64,
     username: String,
     password: String,
     encryption_key: String,
     device_name: String,
     notify_email: String,
-    includes: Vec<String>,
-    excludes: Vec<String>,
-}
-
-#[derive(Default)]
-struct FileStats {
-    considered_for_backup: usize,
-    backed_up_now: usize,
-    already_present: usize,
-    failed_to_backup: usize,
+    includes: Vec<PathBuf>,
+    excludes: Vec<PathBuf>,
 }
 
 fn read_config() -> Result<Config, Error> {
@@ -48,22 +156,6 @@ fn read_config() -> Result<Config, Error> {
     let config = from_yaml_reader(config_file)?;
 
     Ok(config)
-}
-
-fn read_last_run() -> Result<SystemTime, Error> {
-    let string = read_to_string("lastrun")?;
-    let duration = Duration::from_secs(string.parse::<u64>()?);
-
-    Ok(SystemTime::UNIX_EPOCH + duration)
-}
-
-fn write_last_run(last_run: SystemTime) -> Result<(), Error> {
-    let duration = last_run.duration_since(SystemTime::UNIX_EPOCH).unwrap();
-    let string = duration.as_secs().to_string();
-
-    write("lastrun", string)?;
-
-    Ok(())
 }
 
 fn download_util() -> Result<(), Error> {
@@ -97,13 +189,6 @@ fn download_util() -> Result<(), Error> {
     set_permissions("idevsutil_dedup", Permissions::from_mode(0o755))?;
 
     Ok(())
-}
-
-fn make_arg<S: AsRef<OsStr>>(pre: &str, val: S) -> OsString {
-    let mut arg = OsString::new();
-    arg.push(pre);
-    arg.push(val);
-    arg
 }
 
 fn run_util<I, S>(config: &Config, args: I) -> Result<String, Error>
@@ -170,6 +255,7 @@ fn get_server_ip(config: &Config) -> Result<String, Error> {
     }
 
     let srv_ip = parse_tree::<ServerIP>(output)?;
+
     Ok(srv_ip.val)
 }
 
@@ -219,7 +305,16 @@ fn get_quota(config: &Config, srv_ip: &str) -> Result<(u64, u64), Error> {
     }
 
     let quota = parse_tree::<Quota>(output)?;
+
     Ok((quota.used, quota.total))
+}
+
+#[derive(Default)]
+struct FileStats {
+    considered_for_backup: usize,
+    backed_up_now: usize,
+    already_present: usize,
+    failed_to_backup: usize,
 }
 
 #[allow(clippy::borrowed_box)]
@@ -227,7 +322,6 @@ fn upload_files<I, P>(
     config: &Config,
     srv_ip: &str,
     dev_id: &str,
-    logger: &Box<dyn Log>,
     stats: &mut FileStats,
     files: I,
 ) -> Result<(), Error>
@@ -295,19 +389,19 @@ where
 
         if transfer.type_ == "FULL" || transfer.type_ == "INCREMENTAL" {
             let (size, unit) = format_size(transfer_size);
-            logger.info(format_args!(
+            info!(
                 "Transferred {:.1} {} at {} to backup file /{}",
                 size, unit, transfer.rate, transfer.file_name
-            ));
+            );
 
             stats.backed_up_now += 1
         } else if transfer.type_ == "FILE IN SYNC" {
             stats.already_present += 1
         } else {
-            logger.error(format_args!(
+            error!(
                 "Failed to backup file {} due to: {}",
                 transfer.file_name, transfer.type_
-            ));
+            );
 
             stats.failed_to_backup += 1
         }
@@ -316,121 +410,14 @@ where
     Ok(())
 }
 
-fn prepare() -> Result<(Config, String, String), Error> {
-    let config = read_config()?;
-
-    create_dir_all("logs")?;
-
-    download_util()?;
-
-    let srv_ip = get_server_ip(&config)?;
-    info!("Using server IP {}", srv_ip);
-
-    let dev_id = get_device_id(&config, &srv_ip)?;
-    info!("Using device ID {} ({})", dev_id, config.device_name);
-
-    Ok((config, srv_ip, dev_id))
-}
-
-fn run(config: &Config, srv_ip: &str, dev_id: &str) -> Result<(), Error> {
-    let starttime = SystemTime::now();
-
-    let logger = WriteLogger::new(
-        LevelFilter::Info,
-        Default::default(),
-        File::create(format!("logs/{}.log", format_rfc3339_seconds(starttime)))?,
-    )
-    .as_log();
-    let mut stats = FileStats::default();
-
-    logger.info(format_args!(
-        "Starting backup from {} to {} ({})...",
-        get_hostname()?,
-        config.device_name,
-        dev_id,
-    ));
-
-    let excludes = config.excludes.iter().map(Path::new).collect::<Vec<_>>();
-    let mut paths = config
-        .includes
-        .iter()
-        .map(|include| PathBuf::from(include.clone()))
-        .collect::<Vec<_>>();
-    let mut files = Vec::new();
-
-    while let Some(path) = paths.pop() {
-        let path = match path.canonicalize() {
-            Ok(path) => path,
-            Err(err) => {
-                logger.warn(format_args!(
-                    "Skipping path {} as it appears to be a broken symbolic link: {}",
-                    path.display(),
-                    err
-                ));
-                continue;
-            }
-        };
-
-        if let Some(exclude) = excludes.iter().find(|exclude| path.starts_with(exclude)) {
-            logger.info(format_args!(
-                "Skipping path {} due to exclude {}",
-                path.display(),
-                exclude.display(),
-            ));
-            continue;
-        }
-
-        if path.is_file() {
-            files.push(path);
-
-            if files.len() == 1000 {
-                upload_files(config, srv_ip, dev_id, &logger, &mut stats, &files)?;
-                files.clear();
-            }
-        } else if path.is_dir() {
-            let dir = match path.read_dir() {
-                Ok(dir) => dir,
-                Err(err) => {
-                    logger.warn(format_args!(
-                        "Skipping directory {} as it appears to have been removed: {}",
-                        path.display(),
-                        err
-                    ));
-                    continue;
-                }
-            };
-
-            for entry in dir {
-                let entry = match entry {
-                    Ok(entry) => entry,
-                    Err(err) => {
-                        logger.warn(format_args!(
-                            "Skipping entry in directory {} as it appears to have been removed: {}",
-                            path.display(),
-                            err
-                        ));
-                        continue;
-                    }
-                };
-
-                paths.push(entry.path());
-            }
-        } else {
-            logger.warn(format_args!(
-                "Skipping path {} as it is neither a file nor a directory",
-                path.display()
-            ));
-            continue;
-        }
-    }
-
-    if !files.is_empty() {
-        upload_files(config, srv_ip, dev_id, &logger, &mut stats, &files)?;
-    }
-
-    let endtime = SystemTime::now();
-
-    let (quota_used, quota_total) = get_quota(config, srv_ip)?;
+fn mail_summary(
+    config: &Config,
+    srv_ip: &str,
+    starttime: &DateTime<Local>,
+    endtime: &DateTime<Local>,
+    stats: &FileStats,
+) -> Result<(), Error> {
+    let (quota_used, quota_total) = get_quota(config, srv_ip).context("Failed to get quota")?;
 
     let summary = format!(
         r#"
@@ -445,8 +432,8 @@ Files failed to backup: {files_failed_to_backup}
 Quota used: {quota_used} GB out of {quota_total} GB"#,
         device_name = config.device_name,
         hostname = get_hostname()?,
-        starttime = format_rfc3339_seconds(starttime),
-        endtime = format_rfc3339_seconds(endtime),
+        starttime = starttime,
+        endtime = endtime,
         files_considered_for_backup = stats.considered_for_backup,
         files_backed_up_now = stats.backed_up_now,
         files_already_present = stats.already_present,
@@ -454,12 +441,6 @@ Quota used: {quota_used} GB out of {quota_total} GB"#,
         quota_used = quota_used >> 30,
         quota_total = quota_total >> 30,
     );
-
-    if stats.failed_to_backup != 0 {
-        logger.error(format_args!("Finished incomplete backup: {}", summary));
-    } else {
-        logger.info(format_args!("Finished successful backup: {}", summary));
-    }
 
     let subject = if stats.failed_to_backup != 0 {
         format!(
@@ -493,136 +474,11 @@ Quota used: {quota_used} GB out of {quota_total} GB"#,
     Ok(())
 }
 
-fn limit_log<P: AsRef<Path>>(path: P, lines: usize) -> Result<(), Error> {
-    let output = Command::new("tail")
-        .arg("-n")
-        .arg(&lines.to_string())
-        .arg(path.as_ref())
-        .output()?;
-
-    if !output.status.success() {
-        bail!(
-            "Failed read last {} lines of log file {}",
-            lines,
-            path.as_ref().display()
-        );
-    }
-
-    write(path, output.stdout)?;
-
-    Ok(())
-}
-
-fn archive_logs() -> Result<(), Error> {
-    let deadline = SystemTime::now() - Duration::from_secs(7 * 24 * 60 * 60);
-
-    for entry in read_dir("logs")? {
-        let path = entry?.path();
-
-        if path.extension() != Some(OsStr::from_bytes(b"log")) {
-            continue;
-        }
-
-        let timestamp = parse_rfc3339(&path.file_stem().unwrap().to_string_lossy())?;
-
-        if deadline < timestamp {
-            continue;
-        }
-
-        let file_name = path.strip_prefix("logs").unwrap();
-
-        let status = Command::new("7z")
-            .current_dir("logs")
-            .arg("a")
-            .arg("-mx")
-            .arg("archived.7z")
-            .arg(&file_name)
-            .stdout(Stdio::null())
-            .status()?;
-
-        if !status.success() {
-            bail!("Failed to archive log file {}", file_name.display());
-        }
-
-        remove_file(&path)?;
-
-        info!("Archived log file {}", file_name.display());
-    }
-
-    Ok(())
-}
-
-fn main() {
-    switch_to_work_dir().unwrap();
-
-    let _ = limit_log("main.log", 1000);
-
-    WriteLogger::init(
-        LevelFilter::Info,
-        LogConfig {
-            time_format: Some("%F %T"),
-            ..Default::default()
-        },
-        OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open("main.log")
-            .unwrap(),
-    )
-    .unwrap();
-
-    let (config, srv_ip, dev_id) = loop {
-        match prepare() {
-            Ok(vals) => break vals,
-            Err(err) => {
-                error!("Backup preparation failed: {}", err);
-
-                sleep(Duration::from_secs(300));
-                continue;
-            }
-        }
-    };
-
-    loop {
-        let last_run = read_last_run().ok();
-        let interval = Duration::from_secs(config.interval);
-        let now = SystemTime::now();
-
-        if last_run.map_or(false, |last_run| last_run + interval > now) {
-            sleep(Duration::from_secs(300));
-            continue;
-        }
-
-        info!("Backup started...");
-
-        match run(&config, &srv_ip, &dev_id) {
-            Ok(()) => (),
-            Err(err) => {
-                error!("Backup failed: {}", err);
-
-                sleep(Duration::from_secs(300));
-                continue;
-            }
-        }
-
-        info!("Backup completed.");
-
-        if let Err(err) = write_last_run(now) {
-            error!("Failed to update last run: {}", err);
-        }
-
-        if let Err(err) = archive_logs() {
-            error!("Failed to archive logs: {}", err);
-        }
-    }
-}
-
-fn switch_to_work_dir() -> Result<(), Error> {
-    if let Some(work_dir) = args_os().nth(1) {
-        set_current_dir(work_dir)?;
-    }
-
-    Ok(())
+fn make_arg<S: AsRef<OsStr>>(pre: &str, val: S) -> OsString {
+    let mut arg = OsString::new();
+    arg.push(pre);
+    arg.push(val);
+    arg
 }
 
 #[allow(clippy::useless_let_if_seq)]
@@ -651,26 +507,5 @@ fn format_size(size: u64) -> (f64, &'static str) {
 fn get_hostname() -> Result<String, Error> {
     let mut hostname = String::from_utf8(Command::new("hostname").output()?.stdout)?;
     hostname.pop();
-
     Ok(hostname)
-}
-
-trait LogExt {
-    fn info(&self, args: Arguments<'_>);
-    fn warn(&self, args: Arguments<'_>);
-    fn error(&self, args: Arguments<'_>);
-}
-
-impl LogExt for Log {
-    fn info(&self, args: Arguments<'_>) {
-        self.log(&Record::builder().level(Level::Info).args(args).build());
-    }
-
-    fn warn(&self, args: Arguments<'_>) {
-        self.log(&Record::builder().level(Level::Warn).args(args).build());
-    }
-
-    fn error(&self, args: Arguments<'_>) {
-        self.log(&Record::builder().level(Level::Error).args(args).build());
-    }
 }
